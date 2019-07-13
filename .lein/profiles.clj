2d24bc44034c4871f3b317f6dd3ab6588b76aaae
Ilya Bernshteyn <ilya.bernshteyn@rate.com>
2019-07-13 14:22:22 -0500

    Cleaned up broken code and comments

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,16 +1,3 @@
-{:user {
-        :plugins [[lein-ancient "0.6.15"]
-                  [lein-autoreload "0.1.1"]
-                  [venantius/ultra "0.5.2"]]
-        :repl-options
-        {:init
-         (do
-           (set! *print-length* 100))}}}
-                  ;; [lein-describe "0.3.0-SNAPSHOT"]
-                  ;; [lein-difftest "2.0.0"]
-                  ;; [lein-pprint "1.1.1"]
-                  ;; [compojure/lein-template "0.3.0"]
-                  ;; [lein-try "0.4.1"]
-                  ;; [lein-clique "0.1.2"]
-                  ;; [com.jakemccrary/lein-test-refresh "0.3.0"]
-                  ;; [org.clojars.kurtharriger/lein-depgraph "0.1.0"]
+{:user {:plugins [[lein-ancient "0.6.15"]
+                  ;;[lein-autoreload "0.1.1"]
+                  [venantius/ultra "0.5.2"]]}}
